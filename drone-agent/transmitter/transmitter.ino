b22b4af25bb2f76cf2e81c0d6c10db486b783ab3
savidude <mysticsavy@gmail.com>
2017-08-03 14:41:36 +0530

    Changes to transmitter

--- drone-agent/transmitter/transmitter.ino
+++ drone-agent/transmitter/transmitter.ino
@@ -1,48 +1,25 @@
 #include  <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
-
-int msg[1];
+int msg;
 RF24 radio(9,10);
 const uint64_t pipe = 0xE8E8F0F0E1LL;
-int SW1 = 7;
 
-const int trigPin = 2;
-const int echoPin = 3;
-
-// defines variables
+const int trigPin = 2, echoPin = 3;
 long duration;
-int distance;
 
-void setup() {
-  // put your setup code here, to run once:
+void setup(void){
   Serial.begin(9600);
-  radio.begin(); 
-  radio.openWritingPipe(pipe);
-
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  radio.begin(); radio.openWritingPipe(pipe);
+  pinMode(trigPin, OUTPUT); pinMode(echoPin, INPUT); 
 }
 
-void loop() {
-  // Clears the trigPin
+void loop(void){
+  digitalWrite(trigPin, LOW); delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH); delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance= duration*0.034/2;
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
-
-  //Sends distance data using the transmitter
-  msg[0] = distance;
-  radio.write(msg, 1);
+  duration = pulseIn(echoPin, HIGH, 5400);
+  msg= duration*0.034/2;
+  Serial.println(msg);
+  radio.write(88, 1);
 }
